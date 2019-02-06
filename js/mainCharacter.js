2a3c9ad04dca321b2902d37bc43f8672152b21f7
Yerko <yerkodezutter@gmail.com>
2019-02-06 13:46:37 +0100

    character animetion to all derections

--- js/mainCharacter.js
+++ js/mainCharacter.js
@@ -1,3 +1,7 @@
+const CharBlock = blockSize * 2;
+
+
+
 class new_Character {
   constructor(image) {
     this.image = image;
@@ -6,21 +10,21 @@ class new_Character {
 
   character(name, Tx, Ty) {
     this.name = name;
-    this.Tx = Tx * (blockSize * 2);
-    this.Ty = Ty * (blockSize * 2);
+    this.Tx = Tx * CharBlock;
+    this.Ty = Ty * CharBlock;
 
     const characterblock = document.createElement("canvas")
-    characterblock.width = blockSize * 2;
-    characterblock.height = blockSize * 2;
+    characterblock.width = CharBlock;
+    characterblock.height = CharBlock;
     characterblock
       .getContext("2d")
       .drawImage(
         this.image,
         this.Tx, this.Ty,
-        blockSize * 2, blockSize * 2,
+        CharBlock, CharBlock,
 
         0, 0,
-        blockSize * 2, blockSize * 2
+        CharBlock, CharBlock
       )
     this.charactermap.set(name, characterblock)
   }
@@ -36,15 +40,23 @@ class new_Character {
 
 loadImage('img/mainCharacter.png').then(image => {
 
-  function update() {
-    if(dirX != 0 || dirY != 0){
-     player.position.x += dirX*5;
-     player.position.y += dirY*5;
+  // let lastFrame = 0;
+
+  function update(frame = 0) {
+
+    // const deltaFrame = frame - lastFrame;
+    // lastFrame = frame;
+
+    // console.log(deltaFrame);
+
+    if (dirX != 0 || dirY != 0) {
+      player.position.x += dirX * 2;
+      player.position.y += dirY * 2;
     }
 
     const characters = new new_Character(image);
 
-    characters.character("main", 0, 11);
+    characters.character("main", aniFrames()[0], dirSprite);
 
 
     c.drawImage(tileMap, 0, 0);
@@ -66,22 +78,54 @@ loadImage('img/mainCharacter.png').then(image => {
 
 
 
+// ----- FRAMES -----
+
+let aniPicker;
+let aniCount = 0;
+let aniSpeed = ((aniFrameList[1].frames.length - 1) * 10);
+
+function aniFrames() {
+  if(aniCount <= aniSpeed){
+    aniCount++
+  } else {
+    aniCount = 0;
+  }
+  // console.log(Math.floor(aniCount/20));
+  if (dirX != 0 || dirY != 0) {
+    aniPicker = aniFrameList[1].frames[Math.floor(aniCount/10)]
+  } else {
+    aniPicker = [0, 11]
+  }
+  return aniPicker
+}
+
+
+
+
+
+
+
 
 
 // ----- INPUTS -----
 
 let dirX = 0;
 let dirY = 0;
+let dirSprite = 11;
 
 document.addEventListener('keydown', event => {
   if (event.keyCode === 37) {
     dirX = -1;
+    dirSprite = 9;
   } else if (event.keyCode === 39) {
     dirX = 1;
+    dirSprite = 11;
   } else if (event.keyCode === 40) {
     dirY = 1;
+    dirSprite = 10;
   } else if (event.keyCode === 38) {
     dirY = -1;
+    dirSprite = 8;
   }
 
 });
